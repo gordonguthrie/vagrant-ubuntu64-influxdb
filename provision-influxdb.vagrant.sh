6ff14404c14124eabaf2a0e59dbafd96e4c4c1b9
Gordon Guthrie <gguthrie@basho.com>
2015-01-16 13:37:48 +0000

    Added Grafana web server configuration

--- provision-influxdb.vagrant.sh
+++ provision-influxdb.vagrant.sh
@@ -73,11 +73,124 @@ case "$1" in
       cd ${PWD}
       ;;
    *)
-     echo "whatevs"
+     echo "you can start simpleHTTPServer for grafana with start - but after that yer on yer own"
 esac
 EOF
     chmod +x /etc/init.d/grafana
     sudo update-rc.d influxdb defaults
+}
+
+configure_grafana_to_use_influxdb_fn() {
+cat > /home/vagrant/grafana/grafana-1.9.1/config.js <<"EOF"
+// == Configuration
+// config.js is where you will find the core Grafana configuration. This file contains parameter that
+// must be set before Grafana is run for the first time.
+
+define(['settings'], function(Settings) {
+  
+
+  return new Settings({
+
+      /* Data sources
+      * ========================================================
+      * Datasources are used to fetch metrics, annotations, and serve as dashboard storage
+      *  - You can have multiple of the same type.
+      *  - grafanaDB: true    marks it for use for dashboard storage
+      *  - default: true      marks the datasource as the default metric source (if you have multiple)
+      *  - basic authentication: use url syntax http://username:password@domain:port
+      */
+
+      // InfluxDB example setup (the InfluxDB databases specified need to exist)
+      
+      datasources: {
+        influxdb: {
+          type: 'influxdb',
+          url: "http://my_influxdb_server:8086/db/testing",
+          username: 'root',
+          password: 'root',
+        },
+        grafana: {
+          type: 'influxdb',
+          url: "http://my_influxdb_server:8086/db/grafana",
+          username: 'root',
+          password: 'root',
+          grafanaDB: true
+        },
+      },
+      
+
+      // Graphite & Elasticsearch example setup
+      /*
+      datasources: {
+        graphite: {
+          type: 'graphite',
+          url: "http://my.graphite.server.com:8080",
+        },
+        elasticsearch: {
+          type: 'elasticsearch',
+          url: "http://my.elastic.server.com:9200",
+          index: 'grafana-dash',
+          grafanaDB: true,
+        }
+      },
+      */
+
+      // OpenTSDB & Elasticsearch example setup
+      /*
+      datasources: {
+        opentsdb: {
+          type: 'opentsdb',
+          url: "http://opentsdb.server:4242",
+        },
+        elasticsearch: {
+          type: 'elasticsearch',
+          url: "http://my.elastic.server.com:9200",
+          index: 'grafana-dash',
+          grafanaDB: true,
+        }
+      },
+      */
+
+      /* Global configuration options
+      * ========================================================
+      */
+
+      // specify the limit for dashboard search results
+      search: {
+        max_results: 100
+      },
+
+      // default home dashboard
+      default_route: '/dashboard/file/default.json',
+
+      // set to false to disable unsaved changes warning
+      unsaved_changes_warning: true,
+
+      // set the default timespan for the playlist feature
+      // Example: "1m", "1h"
+      playlist_timespan: "1m",
+
+      // If you want to specify password before saving, please specify it below
+      // The purpose of this password is not security, but to stop some users from accidentally changing dashboards
+      admin: {
+        password: ''
+      },
+
+      // Change window title prefix from 'Grafana - <dashboard title>'
+      window_title_prefix: 'Grafana - ',
+
+      // Add your own custom panels
+      plugins: {
+        // list of plugin panels
+        panels: [],
+        // requirejs modules in plugins folder that should be loaded
+        // for example custom datasources
+        dependencies: [],
+      }
+
+    });
+});
+EOF
 }
 
 function configure_vagrant_fn() {
@@ -91,6 +204,7 @@ function main {
     configure_influxdb_fn
     install_grafana_fn
     setup_webserver_fn
+    configure_grafana_to_use_influxdb_fn
     configure_vagrant_fn
     echo "Installation finished" >> ${LOGFILE}
 }
